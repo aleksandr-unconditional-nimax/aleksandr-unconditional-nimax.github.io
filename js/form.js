3f44780cb89c31bc0685a4c4837bcde5cb011535
Aleksandr.Unconditional <alex.p@nimax.agency>
2021-10-20 16:56:17 +0300

    form page

---
+++ js/form.js
@@ -0,0 +1,239 @@
+let step1form1 = document.querySelector('#form-1 .item:nth-child(1)');
+let step2form1 = document.querySelector('#form-1 .item:nth-child(2)');
+let step3form1 = document.querySelector('#form-1 .item:nth-child(3)');
+let step4form1 = document.querySelector('#form-1 .item:nth-child(4)');
+let step5form1 = document.querySelector('#form-1 .item:nth-child(5)');
+let step6form1 = document.querySelector('#form-1 .item:nth-child(6)');
+let step7form1 = document.querySelector('#form-1 .item:nth-child(7)');
+
+let step2form2 = document.querySelector('#form-2 .item:nth-child(2)');
+let step3form2 = document.querySelector('#form-2 .item:nth-child(3)');
+let step4form2 = document.querySelector('#form-2 .item:nth-child(4)');
+let step5form2 = document.querySelector('#form-2 .item:nth-child(5)');
+
+step1form1.onclick = function() {
+  step2form1.classList.add('active');
+};
+
+step2form1.onclick = function() {
+  step3form1.classList.add('active');
+};
+
+step3form1.onclick = function() {
+  step4form1.classList.add('active');
+  step5form1.classList.add('active');
+};
+
+step5form1.onclick = function() {
+  step6form1.classList.add('active');
+};
+
+step6form1.onclick = function() {
+  step7form1.classList.add('active');
+};
+
+let radioForm2 = document.form2;
+
+const handleChange = ({ target: { value, name } }) => {
+  const res = `${value} ${name}`;
+  switch (res) {
+    case 'Да contract':
+      step4form2.classList.remove('active');
+      step2form2.classList.add('active');
+      step5form2.classList.remove('active');
+      break;
+    case 'Нет contract':
+      step2form2.classList.remove('active');
+      step4form2.classList.add('active');
+      break;
+
+    case 'Да contractProblem':
+      step4form2.classList.remove('active');
+      step3form2.classList.add('active');
+      break;
+    case 'Нет contractProblem':
+      step3form2.classList.remove('active');
+      step4form2.classList.add('active');
+      break;
+  }
+}
+
+const arr = Array.from(radioForm2).map(i => {
+  if (i.type === 'text' || i.type === 'radio') {
+    i.addEventListener('change', handleChange)
+  }
+})
+
+step4form2.onclick = function() {
+  step5form2.classList.add('active');
+};
+
+step5form2.onclick = function() {
+  document.getElementById('tab-3').click()
+};
+
+/*
+let innForm1 = document.getElementsByName('innForm2');
+
+if(innForm1.value.length == 0)
+  innForm1.value = "Empty";
+*/
+
+
+
+
+/* FORM REGISTRATION Step START */
+let fieldsetForm3 = document.querySelectorAll("#form-3 .item");
+for(let i=0; i<fieldsetForm3.length; i++){fieldsetForm3[i].onclick = function(){this.nextElementSibling.classList.add('active');}}
+/* FORM REGISTRATION Step END */
+
+
+/* SELECT Style START */
+
+const CLASS_NAME_SELECT = 'select';
+const CLASS_NAME_ACTIVE = 'select_show';
+const CLASS_NAME_SELECTED = 'select-option-selected';
+const SELECTOR_ACTIVE = '.select_show';
+const SELECTOR_DATA = '[data-select]';
+const SELECTOR_DATA_TOGGLE = '[data-select="toggle"]';
+const SELECTOR_OPTION_SELECTED = '.select-option-selected';
+
+class CustomSelect {
+  constructor(target, params) {
+    this._elRoot = typeof target === 'string' ? document.querySelector(target) : target;
+    this._params = params || {};
+    if (this._params['options']) {
+      this._elRoot.classList.add(CLASS_NAME_SELECT);
+      this._elRoot.innerHTML = CustomSelect.template(this._params);
+    }
+    this._elToggle = this._elRoot.querySelector(SELECTOR_DATA_TOGGLE);
+    this._elRoot.addEventListener('click', this._onClick.bind(this));
+  }
+  _onClick(e) {
+    const target = e.target;
+    const type = target.closest(SELECTOR_DATA).dataset.select;
+    switch (type) {
+      case 'toggle':
+        this.toggle();
+        break;
+      case 'option':
+        this._changeValue(target);
+        break;
+    }
+  }
+  _update(option) {
+    const selected = this._elRoot.querySelector(SELECTOR_OPTION_SELECTED);
+    if (selected) {
+      selected.classList.remove(CLASS_NAME_SELECTED);
+    }
+    option.classList.add(CLASS_NAME_SELECTED);
+    this._elToggle.textContent = option.textContent;
+    this._elToggle.value = option.dataset['value'];
+    this._elToggle.dataset.index = option.dataset['index'];
+    this._elRoot.dispatchEvent(new CustomEvent('select.change'));
+    this._params.onSelected ? this._params.onSelected(this, option) : null;
+    return option.dataset['value'];
+  }
+  _reset() {
+    const selected = this._elRoot.querySelector(SELECTOR_OPTION_SELECTED);
+    if (selected) {
+      selected.classList.remove(CLASS_NAME_SELECTED);
+    }
+    this._elToggle.textContent = 'Выберите из списка';
+    this._elToggle.value = '';
+    this._elToggle.dataset.index = -1;
+    this._elRoot.dispatchEvent(new CustomEvent('select.change'));
+    this._params.onSelected ? this._params.onSelected(this, null) : null;
+    return '';
+  }
+  _changeValue(option) {
+    if (option.classList.contains(CLASS_NAME_SELECTED)) {
+      return;
+    }
+    this._update(option);
+    this.hide();
+  }
+  show() {
+    document.querySelectorAll(SELECTOR_ACTIVE).forEach(select => {
+      select.classList.remove(CLASS_NAME_ACTIVE);
+    });
+    this._elRoot.classList.add(CLASS_NAME_ACTIVE);
+  }
+  hide() {
+    this._elRoot.classList.remove(CLASS_NAME_ACTIVE);
+  }
+  toggle() {
+    if (this._elRoot.classList.contains(CLASS_NAME_ACTIVE)) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
+  dispose() {
+    this._elRoot.removeEventListener('click', this._onClick);
+  }
+  get value() {
+    return this._elToggle.value;
+  }
+  set value(value) {
+    let isExists = false;
+    this._elRoot.querySelectorAll('.select-option').forEach((option) => {
+      if (option.dataset['value'] === value) {
+        isExists = true;
+        return this._update(option);
+      }
+    });
+    if (!isExists) {
+      return this._reset();
+    }
+  }
+  get selectedIndex() {
+    return this._elToggle.dataset['index'];
+  }
+  set selectedIndex(index) {
+    const option = this._elRoot.querySelector(`.select-option[data-index="${index}"]`);
+    if (option) {
+      return this._update(option);
+    }
+    return this._reset();
+  }
+}
+
+CustomSelect.template = params => {
+  const name = params['name'];
+  const options = params['options'];
+  const targetValue = params['targetValue'];
+  let items = [];
+  let selectedIndex = -1;
+  let selectedValue = '';
+  let selectedContent = 'Выберите из списка';
+  options.forEach((option, index) => {
+    let selectedClass = '';
+    if (option[0] === targetValue) {
+      selectedClass = ' select-option-selected';
+      selectedIndex = index;
+      selectedValue = option[0];
+      selectedContent = option[1];
+    }
+    items.push(`<li class="select-option${selectedClass}" data-select="option" data-value="${option[0]}" data-index="${index}">${option[1]}</li>`);
+  });
+  return `<button type="button" class="select-toggle" name="${name}" value="${selectedValue}" data-select="toggle" data-index="${selectedIndex}">${selectedContent}</button>
+  <div class="select-dropdown">
+    <ul class="select-options">${items.join('')}</ul>
+  </div>`;
+};
+
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.select')) {
+    document.querySelectorAll(SELECTOR_ACTIVE).forEach(select => {
+      select.classList.remove(CLASS_NAME_ACTIVE);
+    });
+  }
+});
+
+
+
+const select1 = new CustomSelect('#select-1');
+const select2 = new CustomSelect('#select-2');
+
+/* SELECT Style END */
