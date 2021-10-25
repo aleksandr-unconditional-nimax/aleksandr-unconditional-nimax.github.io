08dbb0314980bbe2102d70ef32bbb3afe0141fc9
stvoler <stvol@live.ru>
2021-10-25 14:59:57 +0300

    styles and script forms

--- js/form.js
+++ js/form.js
@@ -1,37 +1,71 @@
-let step1form1 = document.querySelector('#form-1 .item:nth-child(1)');
-let step2form1 = document.querySelector('#form-1 .item:nth-child(2)');
-let step3form1 = document.querySelector('#form-1 .item:nth-child(3)');
-let step4form1 = document.querySelector('#form-1 .item:nth-child(4)');
-let step5form1 = document.querySelector('#form-1 .item:nth-child(5)');
-let step6form1 = document.querySelector('#form-1 .item:nth-child(6)');
-let step7form1 = document.querySelector('#form-1 .item:nth-child(7)');
+let step1form1 = document.querySelector('#form-1 .item1');
+let step2form1 = document.querySelector('#form-1 .item2');
+let step3form1 = document.querySelector('#form-1 .item3');
+let step4form1 = document.querySelector('#form-1 .item4');
+let step5form1 = document.querySelector('#form-1 .item5');
+let step6form1 = document.querySelector('#form-1 .item6');
 
-let step2form2 = document.querySelector('#form-2 .item:nth-child(2)');
-let step3form2 = document.querySelector('#form-2 .item:nth-child(3)');
-let step4form2 = document.querySelector('#form-2 .item:nth-child(4)');
-let step5form2 = document.querySelector('#form-2 .item:nth-child(5)');
+let typework0 = document.querySelector('#form-1 .typework0');
+let typework1_1 = document.querySelector('#form-1 .typework1-1');
+let typework1_2 = document.querySelector('#form-1 .typework1-2');
+let typework1_3 = document.querySelector('#form-1 .typework1-3');
+let typework1_4 = document.querySelector('#form-1 .typework1-4');
+
+let typework2_1 = document.querySelector('#form-1 .typework2-1');
+let typework2_2 = document.querySelector('#form-1 .typework2-2');
+let typework2_3 = document.querySelector('#form-1 .typework2-3');
+let typework2_4 = document.querySelector('#form-1 .typework2-4');
+
+let typework3_1 = document.querySelector('#form-1 .typework3-1');
+let typework3_2 = document.querySelector('#form-1 .typework3-2');
+let typework3_3 = document.querySelector('#form-1 .typework3-3');
+let typework3_4 = document.querySelector('#form-1 .typework3-4');
+let typework3_5 = document.querySelector('#form-1 .typework3-5');
+
+let typework4_1 = document.querySelector('#form-1 .typework4-1');
+let typework4_2 = document.querySelector('#form-1 .typework4-2');
+let typework4_3 = document.querySelector('#form-1 .typework4-3');
+let typework4_4 = document.querySelector('#form-1 .typework4-4');
+let typework4_5 = document.querySelector('#form-1 .typework4-5');
+
+let step2form2 = document.querySelector('#form-2 .item2');
+let step3form2 = document.querySelector('#form-2 .item3');
+let step4form2 = document.querySelector('#form-2 .item4');
+let step5form2 = document.querySelector('#form-2 .item5');
+
+let offset = 50;
 
 step1form1.onclick = function() {
   step2form1.classList.add('active');
+  setTimeout(() => window.scroll({ top: (step2form1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
 };
 
 step2form1.onclick = function() {
   step3form1.classList.add('active');
+  setTimeout(() => window.scroll({ top: (step3form1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
 };
 
 step3form1.onclick = function() {
   step4form1.classList.add('active');
+  setTimeout(() => window.scroll({ top: (step4form1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
+};
+
+step4form1.onclick = function() {
   step5form1.classList.add('active');
+  setTimeout(() => window.scroll({ top: (step5form1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
 };
 
 step5form1.onclick = function() {
   step6form1.classList.add('active');
+  setTimeout(() => window.scroll({ top: (step6form1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
 };
 
 step6form1.onclick = function() {
-  step7form1.classList.add('active');
+  typework0.classList.add('active');
+  setTimeout(() => window.scroll({ top: (typework0.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
 };
 
+let radioForm1 = document.form1;
 let radioForm2 = document.form2;
 
 const handleChange = ({ target: { value, name } }) => {
@@ -46,7 +80,6 @@ const handleChange = ({ target: { value, name } }) => {
       step2form2.classList.remove('active');
       step4form2.classList.add('active');
       break;
-
     case 'Да contractProblem':
       step4form2.classList.remove('active');
       step3form2.classList.add('active');
@@ -55,10 +88,85 @@ const handleChange = ({ target: { value, name } }) => {
       step3form2.classList.remove('active');
       step4form2.classList.add('active');
       break;
+
+    case 'typework1 typework':
+      typework2_1.classList.remove('active');
+      typework3_1.classList.remove('active');
+      typework4_1.classList.remove('active');
+      typework1_1.classList.add('active');
+      setTimeout(() => window.scroll({ top: (typework1_1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
+      break;
+    case 'typework2 typework':
+      typework1_1.classList.remove('active');
+      typework3_1.classList.remove('active');
+      typework4_1.classList.remove('active');
+      typework2_1.classList.add('active');
+      setTimeout(() => window.scroll({ top: (typework2_1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
+      break;
+    case 'typework3 typework':
+      typework1_1.classList.remove('active');
+      typework2_1.classList.remove('active');
+      typework4_1.classList.remove('active');
+      typework3_1.classList.add('active');
+      setTimeout(() => window.scroll({ top: (typework3_1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
+      break;
+    case 'typework4 typework':
+      typework1_1.classList.remove('active');
+      typework2_1.classList.remove('active');
+      typework3_1.classList.remove('active');
+      typework4_1.classList.add('active');
+      setTimeout(() => window.scroll({ top: (typework4_1.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
+      break;
+    case 'typework1-1-no tw1-1':
+      typework1_3.classList.remove('active');
+      typework1_2.classList.add('active');
+      setTimeout(() => window.scroll({ top: (typework1_2.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
+      break;
+    case 'typework1-1-yes tw1-1':
+      typework1_2.classList.remove('active');
+      typework1_3.classList.add('active');
+      setTimeout(() => window.scroll({ top: (typework1_3.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);
+      break;
+    case 'typework3-1 reason':
+      step10form1.classList.remove('active');
+      step11form1.classList.remove('active');
+      step12form1.classList.remove('active');
+      step9form1.classList.add('active');
+      break;
+    case 'typework3-2 reason':
+      step9form1.classList.remove('active');
+      step11form1.classList.remove('active');
+      step12form1.classList.remove('active');
+      step10form1.classList.add('active');
+      break;
+    case 'typework3-3 reason':
+      step9form1.classList.remove('active');
+      step10form1.classList.remove('active');
+      step12form1.classList.remove('active');
+      step11form1.classList.add('active');
+      break;
+    case 'typework3-4 reason':
+      step9form1.classList.remove('active');
+      step10form1.classList.remove('active');
+      step11form1.classList.remove('active');
+      step12form1.classList.add('active');
+      break;
+    case 'typework3-5 reason':
+      step9form1.classList.remove('active');
+      step10form1.classList.remove('active');
+      step11form1.classList.remove('active');
+      step12form1.classList.add('active');
+      break;
   }
 }
 
-const arr = Array.from(radioForm2).map(i => {
+const arr1 = Array.from(radioForm1).map(i => {
+  if (i.type === 'text' || i.type === 'radio') {
+    i.addEventListener('change', handleChange)
+  }
+})
+
+const arr2 = Array.from(radioForm2).map(i => {
   if (i.type === 'text' || i.type === 'radio') {
     i.addEventListener('change', handleChange)
   }
@@ -75,8 +183,15 @@ step5form2.onclick = function() {
 
 /* FORM REGISTRATION Step START */
 let fieldsetForm3 = document.querySelectorAll("#form-3 .item:not(.upload)");
-for(let i=0; i<fieldsetForm3.length; i++){fieldsetForm3[i].onclick = function(){this.nextElementSibling.classList.add('active');}}
+for(let i=0; i<fieldsetForm3.length; i++){fieldsetForm3[i].onclick = function(){this.nextElementSibling.classList.add('active');
+setTimeout(() => window.scroll({ top: (this.nextElementSibling.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);}}
 /* FORM REGISTRATION Step END */
+
+/* FORM typework1 Steps START */
+let fieldsetTypework1 = document.querySelectorAll("#form-1 fieldset[name='typework1']");
+for(let i=0; i<fieldsetTypework1.length; i++){fieldsetTypework1[i].onclick = function(){this.nextElementSibling.classList.add('active');
+setTimeout(() => window.scroll({ top: (this.nextElementSibling.offsetTop - offset), left: 0, behavior: 'smooth' }), 100);}}
+/* FORM typework1 Steps END */
 
 
 /* SELECT OPTION Style START */
